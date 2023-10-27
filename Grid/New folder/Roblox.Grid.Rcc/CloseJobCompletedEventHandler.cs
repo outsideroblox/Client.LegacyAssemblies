15c63eec5405b3460dc15664d6f8b6cf7b87803d
EasternBloxxer <ender@astracorp.xyz>
2023-10-28 00:55:02 +0300

    n

--- Grid/New folder/Roblox.Grid.Rcc/CloseJobCompletedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-using System.CodeDom.Compiler;
-using System.ComponentModel;
-
-namespace Roblox.Grid.Rcc;
-
-[GeneratedCode("wsdl", "4.8.3928.0")]
-public delegate void CloseJobCompletedEventHandler(object sender, AsyncCompletedEventArgs e);
--- Grid/New folder/Roblox.Grid.Rcc/RenewLeaseCompletedEventHandler.cs
+++
@@ -1,6 +0,0 @@
-using System.CodeDom.Compiler;
-
-namespace Roblox.Grid.Rcc;
-
-[GeneratedCode("wsdl", "4.8.3928.0")]
-public delegate void RenewLeaseCompletedEventHandler(object sender, RenewLeaseCompletedEventArgs e);
